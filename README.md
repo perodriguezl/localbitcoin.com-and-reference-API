ac5998950ee9cbbbff19f865e71dbb9f8cc1b4c7
Pedro <perodriguezl@gmail.com>
2021-02-15 19:26:17 -0600

    Second Commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # localbitcoin.com-and-reference-API
 This is a project for a script to run the calculations over validations for cripto currencies
+
+## First setup your environment
+- Create `.env` file locally.
+- Copy all the content of `.env.example` file into `.env` file
+- Fill `.env` file with the following variables:
+```bash
+RAPID_API_SECRET_HOST= <<RAPID API SECRETS>>
+RAPID_API_SECRET_KEY= <<RAPID API SECRETS>>
+LOCAL_BTC_HMAC_KEY= <<Localbitcoin.com Secrets>>
+LOCAL_BTC_HMAC_SECRET= <<Localbitcoin.com Secrets>>
+```
+
+RAPID API SECRETS from: 
+https://perodriguezl-cryptocurrency-real-time-v1.p.rapidapi.com
+
+
+## Install Dependencies
+
+`pip install requirements.txt`
+
+## Change the parameters as needed:
+
+in `app.py` change as needed:
+
+```python
+# Search by currencies
+currencies = ['CAD', 'USD']
+
+# Payment Methods to search
+payment_methods = ['payoneer', 'zelle', 'paypal', 'interac-e-transfer']
+
+# Specific search by country and payment method
+countries = [{
+    'code': 'AR',
+    'name': 'ARGENTINA',
+    'currency': 'USD',
+}]
+```
+
+## ENJOY ;)
+
+run the script: `python app.py`
---
+++ app.py
@@ -0,0 +1,120 @@
+from lbcapi import api
+import re
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# START OF VARIABLES
+
+# Search by currencies
+currencies = ['CAD', 'USD']
+
+# Payment Methods to search
+payment_methods = ['payoneer', 'zelle', 'paypal', 'interac-e-transfer']
+
+# Specific search by country and payment method
+countries = [{
+    'code': 'AR',
+    'name': 'ARGENTINA',
+    'currency': 'USD',
+}]
+
+# END OF VARIABLES
+
+RAPID_API_SECRET_HOST = os.getenv("RAPID_API_SECRET_HOST")
+RAPID_API_SECRET_KEY = os.getenv("RAPID_API_SECRET_KEY")
+
+LOCAL_BTC_HMAC_KEY = '016f6074b8bdec33cc138a57366bdc85'
+LOCAL_BTC_HMAC_SECRET = '84c1b8ac986bc2adfc1aed8f7bd01e4eecf25eb5b2c0858d110b032b21e77f7d'
+
+url = "https://perodriguezl-cryptocurrency-real-time-v1.p.rapidapi.com/info/BTC"
+
+headers = {
+    'x-rapidapi-host': RAPID_API_SECRET_HOST,
+    'x-rapidapi-key': RAPID_API_SECRET_KEY
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+value = response.json()
+
+non_decimal = re.compile(r'[^\d.]+')
+
+my_score = 65
+
+
+def interate_over_ads(me_json):
+    trade_count = {}
+    i = 0
+    global value
+    for x in me_json['data']['ad_list']:
+        price = float(x['data']['temp_price_usd'])
+        reference = value['price_usd']
+        rating = int(non_decimal.sub('', x['data']['profile']['trade_count']))
+        score = int(x['data']['profile']['feedback_score'])
+        if rating < 50 or score < 95 or x['data']['require_feedback_score'] > my_score:
+            continue
+        if i == 0:
+            minimum = {
+                'username': x['data']['profile']['username'],
+                'rating': rating,
+                'score': score,
+                'reference_rapidapi': reference,
+                'price': price,
+                'ad': x['actions']['public_view'],
+                'diff': str((price-reference)*100/price),
+                'currency': x['data']['currency'],
+                'method': x['data']['online_provider'],
+                'require_feedback_score': x['data']['require_feedback_score'],
+                'require_trade_volume': x['data']['require_trade_volume'],
+            }
+        else:
+            if float(x['data']['temp_price_usd']) < minimum['price']:
+                minimum = {
+                    'username': x['data']['profile']['username'],
+                    'rating': rating,
+                    'score': score,
+                    'reference_rapidapi': reference,
+                    'price': price,
+                    'ad': x['actions']['public_view'],
+                    'diff': str((price-reference)*100/price),
+                    'method': x['data']['online_provider'],
+                    'currency': x['data']['currency'],
+                    'require_feedback_score': x['data']['require_feedback_score'],
+                    'require_trade_volume': x['data']['require_trade_volume'],
+                }
+        i = i + 1
+    print(minimum)
+
+conn = api.hmac(LOCAL_BTC_HMAC_KEY, LOCAL_BTC_HMAC_SECRET)
+
+print('by currency')
+print(currencies)
+
+for currency in currencies:
+    me_json = conn.call('GET', '/buy-bitcoins-online/{}/.json'.format(currency)).json()
+    interate_over_ads(me_json)
+
+print('by payment methods')
+print(payment_methods)
+
+payment_methods_list = conn.call('GET', '/api/payment_methods/').json()
+
+countries_list = conn.call('GET', '/api/countrycodes/').json()
+
+currencies_list = conn.call('GET', '/api/currencies/').json()
+
+for payment in payment_methods:
+    me_json = conn.call('GET', '/buy-bitcoins-online/{}/.json'.format(payment)).json()
+    interate_over_ads(me_json)
+
+print(countries)
+
+for country in countries:
+    string = 'https://localbitcoins.com/buy-bitcoins-online/{}/{}/{}/.json'.format(country['code'], country['name'], 'national-bank-transfer')
+    me_json = requests.get(string).json()
+    for x in me_json['data']['ad_list']:
+        if x['data']['currency'] != country['currency']:
+            me_json['data']['ad_list'].remove(x)
+    interate_over_ads(me_json)
